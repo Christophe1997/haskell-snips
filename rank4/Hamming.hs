ad393f141e7b9ebcce0e8303c04450c43f311295
Christophe1997 <hey_christophe@outlook.com>
2020-07-10 15:47:38 +0800

    Update: have better solutions

---
+++ rank4/Hamming.hs
@@ -0,0 +1,19 @@
+module Hamming where
+
+hamming  :: Int -> Int
+hamming = ((0 : 1 : [x | x <- [2..], isHamming x]) !!)
+  where isHamming x
+          | x == 1         = True
+          | x `mod` 5 == 0 = isHamming $ x `div` 5
+          | x `mod` 3 == 0 = isHamming $ x `div` 3
+          | x `mod` 2 == 0 = isHamming $ x `div` 2
+          | otherwise      = False
+
+-- A better solution
+-- hamming  :: Int -> Int
+-- hamming n = seq !! (n - 1)
+--   where seq = 1 : map (* 2) seq `join` map (* 3) seq `join` map (* 5) seq
+--         join xxs@(x : xs) yys@(y : ys)
+--           | x > y     = y : join xxs ys
+--           | x < y     = x : join xs yys
+--           | otherwise = x : join xs ys
